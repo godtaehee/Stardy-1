d3fc322c347109bd40afcc438d7ef1c05ba24633
Taehee Kim <44861205+godtaehee@users.noreply.github.com>
2021-06-17 09:20:07 +0900

    Update StudyServiceImpl.java

--- workspace/Stardy/src/com/stardy/service/StudyServiceImpl.java
+++ workspace/Stardy/src/com/stardy/service/StudyServiceImpl.java
@@ -5,36 +5,30 @@ import com.stardy.entity.view.StudyRegisterView;
 import com.stardy.entity.view.StudyView;
 import com.stardy.util.DatabaseUtil;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-
-
-import javax.security.auth.message.callback.PrivateKeyCallback.Request;
-
 public class StudyServiceImpl implements StudyService {
-	
+
 	public List<StudyRegisterView> getMyStudyList(int memberIdParam) {
-		
+
 		List<StudyRegisterView> list = new ArrayList<>();
-		
-		if(memberIdParam == 0) 
+
+		if (memberIdParam == 0)
 			return list;
-		
+
 		String sql = "SELECT * FROM STUDY_REGISTER_VIEW WHERE MEMBER_ID=" + memberIdParam;
-		
-		try(
-				Connection con = DatabaseUtil.getConnection();
-				Statement st = con.createStatement();
-				ResultSet rs = st.executeQuery(sql);
-				) {
-			
-			while(rs.next()) {
-				
+
+		try (
+			Connection con = DatabaseUtil.getConnection();
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+		) {
+
+			while (rs.next()) {
+
 				int studyId = rs.getInt("STUDY_ID");
 				int memberId = rs.getInt("MEMBER_ID");
 				int id = rs.getInt("ID");
@@ -51,39 +45,39 @@ public class StudyServiceImpl implements StudyService {
 				String path = rs.getString("PATH");
 				int cnt = rs.getInt("CNT");
 				String name = rs.getString("NAME");
-				
+
 				StudyRegisterView study = new StudyRegisterView(studyId, memberId, id, title, leader, categoryId, limit, open,
-						dueDate, intro, regDate, updateDate, bg, path, cnt, name);
-				
+					dueDate, intro, regDate, updateDate, bg, path, cnt, name);
+
 				list.add(study);
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return list;
 
 	}
-	
-	public List<StudyRegisterView>  getNotMyStudyList(int memberIdParam) {
-		
+
+	public List<StudyRegisterView> getNotMyStudyList(int memberIdParam) {
+
 		List<StudyRegisterView> list = new ArrayList<>();
-		
-		if(memberIdParam == 0) 
+
+		if (memberIdParam == 0)
 			return list;
-		
+
 		String sql = "SELECT * FROM STUDY_REGISTER_VIEW WHERE NOT MEMBER_ID IN " + memberIdParam;
-		
-		try(
-				Connection con = DatabaseUtil.getConnection();
-				Statement st = con.createStatement();
-				ResultSet rs = st.executeQuery(sql);
-				) {
-			
-			while(rs.next()) {
-				
+
+		try (
+			Connection con = DatabaseUtil.getConnection();
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+		) {
+
+			while (rs.next()) {
+
 				int studyId = rs.getInt("STUDY_ID");
 				int memberId = rs.getInt("MEMBER_ID");
 				int id = rs.getInt("ID");
@@ -100,130 +94,129 @@ public class StudyServiceImpl implements StudyService {
 				String path = rs.getString("PATH");
 				int cnt = rs.getInt("CNT");
 				String name = rs.getString("NAME");
-				
+
 				StudyRegisterView study = new StudyRegisterView(studyId, memberId, id, title, leader, categoryId, limit, open,
-						dueDate, intro, regDate, updateDate, bg, path, cnt, name);
-				
+					dueDate, intro, regDate, updateDate, bg, path, cnt, name);
+
 				list.add(study);
-				
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return list;
 
 	}
 
-    
-    public int getCrnt(Study study) throws SQLException {
+
+	public int getCrnt(Study study) throws SQLException {
 
 		String sql = "SELECT COUNT(MEMBER_ID) CNT FROM JOINED_STUDY WHERE STUDY_ID=" + study.getId();
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        con = DatabaseUtil.getConnection();
-        pstmt = con.prepareStatement(sql);
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		con = DatabaseUtil.getConnection();
+		pstmt = con.prepareStatement(sql);
 		ResultSet rs = pstmt.executeQuery();
-		
+
 		rs.next();
-		
-        int cnt = rs.getInt("CNT");
-        
-        pstmt.close();
-        con.close();
-        rs.close();
-    		
-    	
-    	return cnt;
-             
-    }
-    
-    public String getLeader(int id) throws SQLException {
-		
+
+		int cnt = rs.getInt("CNT");
+
+		pstmt.close();
+		con.close();
+		rs.close();
+
+
+		return cnt;
+
+	}
+
+	public String getLeader(int id) throws SQLException {
+
 		String sql = "SELECT NICKNAME FROM MEMBER WHERE ID=" + id;
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        con = DatabaseUtil.getConnection();
-        pstmt = con.prepareStatement(sql);
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		con = DatabaseUtil.getConnection();
+		pstmt = con.prepareStatement(sql);
 		ResultSet rs = pstmt.executeQuery();
-		
+
 		rs.next();
-		
-        String nickName = rs.getString("NICKNAME");
-        
-        pstmt.close();
-        con.close();
-        rs.close();
-    		
-    	
-    	return nickName;
-             
-    }
-    
-    public boolean isLeader(int memberId, int studyId) throws SQLException {
-		
-  		String sql = "SELECT * FROM STUDY WHERE MEMBER_ID=" + memberId + " AND ID=" + studyId;
-          Connection con = null;
-          PreparedStatement pstmt = null;
-          con = DatabaseUtil.getConnection();
-          pstmt = con.prepareStatement(sql);
-  		ResultSet rs = pstmt.executeQuery();
-  		
-  		boolean flag = false;
-  		if(rs.next()) {
-  			flag = true;
-  		}
-  		
-          pstmt.close();
-          con.close();
-          rs.close();
-      		
-      	
-      	return flag;
-               
-      }
-    
-    public boolean isMember(int memberId, int studyId) throws SQLException {
-    	
-    	String sql = "SELECT * FROM JOINED_STUDY WHERE MEMBER_ID="+memberId+" AND STUDY_ID="+studyId;
-    	System.out.println(sql);
-  		System.out.println("스터디아이디 멤버아이디 " +memberId + " " + studyId);
-        Connection con = null;
-        Statement st = null;
-        con = DatabaseUtil.getConnection();
-        st = con.createStatement();
-    		ResultSet rs = st.executeQuery(sql);
-    		
-    		boolean flag = false;
-    		if(rs.next()) {
-    			System.out.println("됬냐안됬");
-    			flag = true;
-    		}
-    		
-            st.close();
-            con.close();
-            rs.close();
-        		
-        	
-        	return flag;
-    	
-    }
+
+		String nickName = rs.getString("NICKNAME");
+
+		pstmt.close();
+		con.close();
+		rs.close();
 
 
-    public StudyRegisterView getStudy(int sId) throws SQLException {
-        String sql = "SELECT * FROM STUDY_REGISTER_VIEW WHERE ID=?";
+		return nickName;
 
-        Connection con = null;
-        PreparedStatement pstmt = null;
+	}
 
-        con = DatabaseUtil.getConnection();
-        pstmt = con.prepareStatement(sql);
-        pstmt.setInt(1, sId);
-        ResultSet rs = pstmt.executeQuery();
+	public boolean isLeader(int memberId, int studyId) throws SQLException {
 
-        if(rs.next()) {
-        	int studyId = rs.getInt("STUDY_ID");
+		String sql = "SELECT * FROM STUDY WHERE MEMBER_ID=" + memberId + " AND ID=" + studyId;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		con = DatabaseUtil.getConnection();
+		pstmt = con.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
+
+		boolean flag = false;
+		if (rs.next()) {
+			flag = true;
+		}
+
+		pstmt.close();
+		con.close();
+		rs.close();
+
+
+		return flag;
+
+	}
+
+	public boolean isMember(int memberId, int studyId) throws SQLException {
+
+		String sql = "SELECT * FROM JOINED_STUDY WHERE MEMBER_ID=" + memberId + " AND STUDY_ID=" + studyId;
+		System.out.println(sql);
+		System.out.println("스터디아이디 멤버아이디 " + memberId + " " + studyId);
+		Connection con = null;
+		Statement st = null;
+		con = DatabaseUtil.getConnection();
+		st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+
+		boolean flag = false;
+		if (rs.next()) {
+			System.out.println("됬냐안됬");
+			flag = true;
+		}
+
+		st.close();
+		con.close();
+		rs.close();
+
+		return flag;
+
+	}
+
+
+	public StudyRegisterView getStudy(int sId) throws SQLException {
+		String sql = "SELECT * FROM STUDY_REGISTER_VIEW WHERE ID=?";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		con = DatabaseUtil.getConnection();
+		pstmt = con.prepareStatement(sql);
+		pstmt.setInt(1, sId);
+		ResultSet rs = pstmt.executeQuery();
+
+		if (rs.next()) {
+			int studyId = rs.getInt("STUDY_ID");
 			int memberId = rs.getInt("MEMBER_ID");
 			int id = rs.getInt("ID");
 			String title = rs.getString("TITLE");
@@ -239,220 +232,212 @@ public class StudyServiceImpl implements StudyService {
 			String path = rs.getString("PATH");
 			int cnt = rs.getInt("CNT");
 			String name = rs.getString("NAME");
-			
+
 			StudyRegisterView study = new StudyRegisterView(studyId, memberId, id, title, leader, categoryId, limit, open,
-					dueDate, intro, regDate, updateDate, bg, path, cnt, name);
-			
-            pstmt.close();
-            con.close();
-            rs.close();
+				dueDate, intro, regDate, updateDate, bg, path, cnt, name);
 
-            return study;
-        }
+			pstmt.close();
+			con.close();
+			rs.close();
 
-        return null;
+			return study;
+		}
 
-    }
+		return null;
 
-
-    public int getDuringTime(int id) throws SQLException {
-        //
-        String sql = "SELECT TRUNC(DUEDATE) - TRUNC(REGDATE) AS TIMES FROM STUDY WHERE ID=?";
-
-        Connection con = null;
-        PreparedStatement pstmt = null;
-
-        con = DatabaseUtil.getConnection();
-        pstmt = con.prepareStatement(sql);
-        pstmt.setInt(1, id);
-        ResultSet rs = pstmt.executeQuery();
-
-        int time = 0;
-
-        if(rs.next()) {
-            time = Integer.parseInt(rs.getString("TIMES"));
-        }
-
-        pstmt.close();
-        con.close();
-        rs.close();
-
-        return time;
-    }
-
-    
-    public int getStudyId(int memberId, String title) {
-    	
- 	   String sql = "SELECT ID FROM STUDY WHERE MEMBER_ID=" + memberId +" AND TITLE='"+title+"'";
+	}
 
 
- 	   int id = 0;
-        
+	public int getDuringTime(int id) throws SQLException {
 
-		   
-	        try {
-	            Connection con = DatabaseUtil.getConnection();
-		        PreparedStatement pstmt = con.prepareStatement(sql);
-		        ResultSet rs = pstmt.executeQuery();
-		        
-		        if(rs.next()) {
-		        	id = rs.getInt("ID");
-		        }
-		        
-		        pstmt.close();
-		        con.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		String sql = "SELECT TRUNC(DUEDATE) - TRUNC(REGDATE) AS TIMES FROM STUDY WHERE ID=?";
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		con = DatabaseUtil.getConnection();
+		pstmt = con.prepareStatement(sql);
+		pstmt.setInt(1, id);
+		ResultSet rs = pstmt.executeQuery();
+
+		int time = 0;
+
+		if (rs.next()) {
+			time = Integer.parseInt(rs.getString("TIMES"));
+		}
+
+		pstmt.close();
+		con.close();
+		rs.close();
+
+		return time;
+	}
+
+
+	public int getStudyId(int memberId, String title) {
+
+		String sql = "SELECT ID FROM STUDY WHERE MEMBER_ID=" + memberId + " AND TITLE='" + title + "'";
+
+
+		int id = 0;
+
+
+		try {
+			Connection con = DatabaseUtil.getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				id = rs.getInt("ID");
 			}
 
-
- 	
- 	return id;
- }
-
-    
-    public void insertJoinedStudy(int memberId, String title) {
-    	
-		int studyId =  getStudyId(memberId, title);
-	
-	   String sql = "INSERT INTO JOINED_STUDY(STUDY_ID, MEMBER_ID) VALUES (?, ?)";
-
-
-		
-	   int flag = 0;
-	   
-        try {
-            Connection con = DatabaseUtil.getConnection();
-	        PreparedStatement pstmt = con.prepareStatement(sql);
-			pstmt.setInt(1, studyId);
-	        pstmt.setInt(2, memberId);
-	        flag = pstmt.executeUpdate();
-	        pstmt.close();
-	        con.close();
+			pstmt.close();
+			con.close();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
-    }
-	
-//페이징 처리 리스트 가져오기 
-   
-   @Override
-   public List<StudyView> getList() {
-      
-      return getList("","", 1);
-   }
 
 
-   @Override
-   public List<StudyView> getList(int page) {
+		return id;
+	}
 
 
-      return getList("","", page);
+	public void insertJoinedStudy(int memberId, String title) {
 
-   }
+		int studyId = getStudyId(memberId, title);
+
+		String sql = "INSERT INTO JOINED_STUDY(STUDY_ID, MEMBER_ID) VALUES (?, ?)";
+
+		try {
+			Connection con = DatabaseUtil.getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, studyId);
+			pstmt.setInt(2, memberId);
+			flag = pstmt.executeUpdate();
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+//페이징 처리 리스트 가져오기
+
+	@Override
+	public List<StudyView> getList() {
+
+		return getList("", "", 1);
+	}
 
 
-   @Override
-   public List<StudyView> getList(String category, String query, int page) {
-      System.out.println("카테고리 프린트 : "+category);
-      List<StudyView> list = new ArrayList<>();
-      int count=0;
-      String sql = "SELECT * " + "FROM (SELECT ROWNUM AS rnum, A.*"
-            + "FROM (SELECT * FROM study_view WHERE CATEGORY_ID like '%'||?||'%' AND TITLE LIKE '%'||?||'%' ORDER BY ID DESC) A)"
-            + "WHERE rnum >= ? AND rnum <=?";
+	@Override
+	public List<StudyView> getList(int page) {
 
-      Connection con = DatabaseUtil.getConnection();
 
-      try {
-         PreparedStatement pstmt = con.prepareStatement(sql);
-         int startPage = (page - 1) * 10 + 1;
-         int endPage = startPage + 10 - 1;
-         
-         pstmt.setString(1, category);
-         pstmt.setString(2, query);
-         pstmt.setInt(3, startPage);
-         pstmt.setInt(4, endPage);
-         ResultSet rs = pstmt.executeQuery();
+		return getList("", "", page);
 
-         while (rs.next()) {
+	}
 
-            int id = rs.getInt("ID");
-            String title = rs.getString("TITLE");
-            String intro = rs.getString("INTRO");
-            String open = rs.getString("OPEN");
-            String limit = rs.getString("LIMIT");
 
-            Date regDate = rs.getDate("REGDATE");
-            Date updateDate = rs.getDate("UPDATEDATE");
-            Date dueDate = rs.getDate("DUEDATE");
+	@Override
+	public List<StudyView> getList(String category, String query, int page) {
+		System.out.println("카테고리 프린트 : " + category);
+		List<StudyView> list = new ArrayList<>();
+		int count = 0;
+		String sql = "SELECT * " + "FROM (SELECT ROWNUM AS rnum, A.*"
+			+ "FROM (SELECT * FROM study_view WHERE CATEGORY_ID like '%'||?||'%' AND TITLE LIKE '%'||?||'%' ORDER BY ID DESC) A)"
+			+ "WHERE rnum >= ? AND rnum <=?";
 
-            String bg = rs.getString("BG");
-            String path = rs.getString("PATH");
+		Connection con = DatabaseUtil.getConnection();
 
-            int memberId = rs.getInt("MEMBER_ID");
-            int categoryId = rs.getInt("CATEGORY_ID");
+		try {
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			int startPage = (page - 1) * 10 + 1;
+			int endPage = startPage + 10 - 1;
 
-            int cnt = rs.getInt("CNT");
-            String name = rs.getString("NAME");
+			pstmt.setString(1, category);
+			pstmt.setString(2, query);
+			pstmt.setInt(3, startPage);
+			pstmt.setInt(4, endPage);
+			ResultSet rs = pstmt.executeQuery();
 
-            StudyView studyView = StudyView.builder().id(id).title(title).intro(intro).open(open).limit(limit)
-                  .regDate(regDate).updateDate(updateDate).dueDate(dueDate).bg(bg).path(path).memberId(memberId)
-                  .categoryId(categoryId).cnt(cnt).name(name).build();
+			while (rs.next()) {
 
-            list.add(studyView);
-            count++;
-            System.out.println("디비ㅇㅇ count 수"+count);
-            
-         }
+				int id = rs.getInt("ID");
+				String title = rs.getString("TITLE");
+				String intro = rs.getString("INTRO");
+				String open = rs.getString("OPEN");
+				String limit = rs.getString("LIMIT");
 
-         rs.close();
-         pstmt.close();
-         con.close();
+				Date regDate = rs.getDate("REGDATE");
+				Date updateDate = rs.getDate("UPDATEDATE");
+				Date dueDate = rs.getDate("DUEDATE");
 
-      } catch (SQLException e) {
+				String bg = rs.getString("BG");
+				String path = rs.getString("PATH");
 
-         e.printStackTrace();
-      }
+				int memberId = rs.getInt("MEMBER_ID");
+				int categoryId = rs.getInt("CATEGORY_ID");
 
-      return list;
+				int cnt = rs.getInt("CNT");
+				String name = rs.getString("NAME");
 
-   }
-   
-   // 총 레코드 수 구하기
-   public int paging() {
-      return paging("", "");
-   }
-      
-   @Override
-   public int paging(String category, String query) {
+				StudyView studyView = StudyView.builder().id(id).title(title).intro(intro).open(open).limit(limit)
+					.regDate(regDate).updateDate(updateDate).dueDate(dueDate).bg(bg).path(path).memberId(memberId)
+					.categoryId(categoryId).cnt(cnt).name(name).build();
 
-      Connection con = DatabaseUtil.getConnection();
-      System.out.println("c:"+category +", q:"+query);
-      String sql = "select count(*) count from study_view where category_id like '%'||?||'%'  and title like '%'||?||'%' ";
-      System.out.println(sql);
-      ResultSet rs = null;
-      int totalContent = 0;
+				list.add(studyView);
+				count++;
+				System.out.println("디비ㅇㅇ count 수" + count);
 
-      try {
-         PreparedStatement pstmt = con.prepareStatement(sql);
-         pstmt.setString(1, category);
-         pstmt.setString(2, query);
-         rs = pstmt.executeQuery();
-         while (rs.next()) {
-            System.out.println("enter");
-            totalContent = rs.getInt("count");
-         }
-      } catch (SQLException e) {
+			}
 
-         e.printStackTrace();
-      }
+			rs.close();
+			pstmt.close();
+			con.close();
 
-      // 총 페이지 수
+		} catch (SQLException e) {
 
-      return totalContent;
-   }
+			e.printStackTrace();
+		}
+
+		return list;
+
+	}
+
+	// 총 레코드 수 구하기
+	public int paging() {
+		return paging("", "");
+	}
+
+	@Override
+	public int paging(String category, String query) {
+
+		Connection con = DatabaseUtil.getConnection();
+		System.out.println("c:" + category + ", q:" + query);
+		String sql = "select count(*) count from study_view where category_id like '%'||?||'%'  and title like '%'||?||'%' ";
+		System.out.println(sql);
+		ResultSet rs = null;
+		int totalContent = 0;
+
+		try {
+			PreparedStatement pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, category);
+			pstmt.setString(2, query);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				System.out.println("enter");
+				totalContent = rs.getInt("count");
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		// 총 페이지 수
+
+		return totalContent;
+	}
 
 }
